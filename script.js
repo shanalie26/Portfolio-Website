f581a0e27918a942d6471547b7fa60f2a63c3d35
shanalie26 <shanalie01@outlook.com>
2025-12-08 19:14:21 +0530

    deleting skill section

--- script.js
+++ script.js
@@ -5,27 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-// ----------------------------------------
-// Scroll Animation for Skills (One by One)
-// ----------------------------------------
-
-const skillCards = document.querySelectorAll(".skill-card");
-
-function revealSkills() {
-    let delay = 0;
-
-    skillCards.forEach((card) => {
-        const cardTop = card.getBoundingClientRect().top;
-        const triggerPoint = window.innerHeight - 80; 
-
-        if (cardTop < triggerPoint) {
-            setTimeout(() => {
-                card.classList.add("visible");
-            }, delay);
-
-            delay += 150; // delay between each card
-        }
-    });
-}
-
-window.addEventListener("scroll", revealSkills);
